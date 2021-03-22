f47b06c27e59282fd182bf5986df99ca222f4cff
cyleung428 <msf96a85@gmail.com>
2021-03-22 23:13:20 +0800

    Feat: detect two face similarity

--- client/src/components/Vote.js
+++ client/src/components/Vote.js
@@ -5,6 +5,7 @@ import {
     DocumentCardTitle,
     DocumentCardDetails,
 } from 'office-ui-fabric-react/lib/DocumentCard';
+import * as faceapi from 'face-api.js';
 
 const pageStyle = mergeStyles({
     alignItems: "center",
@@ -18,13 +19,13 @@ const Vote = (props) => {
     const [voterInfo, setVoterInfo] = useState(null);
     const [running, setRunning] = useState(false);
     const [candidates, setCandidates] = useState([]);
-    const [video,]=useState(React.createRef());
+    const [video,] = useState(React.createRef());
 
-    const videoError=(error)=>{
-        console.log("error",error);
+    const videoError = (error) => {
+        console.log("error", error);
     }
 
-    const handleVideo=(stream)=>{
+    const handleVideo = (stream) => {
         video.current.srcObject = stream;
     }
 
@@ -37,29 +38,32 @@ const Vote = (props) => {
             return;
         }
 
-        // const options = new faceApi.TinyFaceDetectorOptions({
-        //   inputSize: 512,
-        //   scoreThreshold: 0.5
-        // });
+        const options = new faceapi.TinyFaceDetectorOptions({
+          inputSize: 512,
+          scoreThreshold: 0.5
+        });
+        
 
-        // const result = await faceApi
-        //   .detectSingleFace(this.video.current, options)
-        //   .withFaceExpressions();
+        const result = await faceapi
+          .detectAllFaces(video.current, options)
+          .withFaceExpressions();
 
-        // if (result) {
-        //   this.log(result);
-        //   const expressions = result.expressions.reduce(
-        //     (acc, { expression, probability }) => {
-        //       acc.push([expressionMap[expression], probability]);
-        //       return acc;
-        //     },
-        //     []
-        //   );
-        //   this.log(expressions);
-        //   this.setState(() => ({ expressions }));
-        // }
+        if (result) {
+          console.log(result);
+        }
 
-        setTimeout(() => onPlay(), 1000);
+        if (result.length >1) {
+            const canvases = await faceapi.extractFaces(video.current, result.map(result=>result.detection))
+            // console.log(canvases)
+            const faceDes1 = await faceapi.computeFaceDescriptor(canvases[0])
+            const faceDes2 = await faceapi.computeFaceDescriptor(canvases[1])
+            const distance = faceapi.utils.round(
+                faceapi.euclideanDistance(faceDes1, faceDes2)
+            )
+            console.log(distance)
+        }
+
+        setTimeout(() => onPlay(), 2000);
     };
 
     const getVoterInfo = async () => {
@@ -84,11 +88,17 @@ const Vote = (props) => {
     }
 
     useEffect(() => {
+        async function loadModels() {
+            await faceapi.loadFaceRecognitionModel("/models");
+            await faceapi.nets.tinyFaceDetector.loadFromUri("/models");
+            await faceapi.loadFaceExpressionModel("/models");
+        }
+        loadModels();
         getVoterInfo(electionInstance);
         getAllCandidates(electionInstance);
         navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
         if (navigator.getUserMedia) {
-            navigator.getUserMedia({video: true}, handleVideo, videoError);
+            navigator.getUserMedia({ video: true }, handleVideo, videoError);
         }
     }, [electionInstance, account])
 
@@ -110,51 +120,50 @@ const Vote = (props) => {
 
 
     return (
-        <>
+        <div style={{ width: "100%" }}>
             {
                 isAdmin ?
                     <div>
                         Only voters can vote
-                    </div>:
-                    <div style={{ width: "100%", height: "60vh"}}>
-                    <video
-                        ref={video}
-                        autoPlay
-                        muted
-                        onPlay={onPlay}
-                        style={{
-                            width: "100%",
-                            height: "50vh",
-                            left: 0,
-                            right: 0,
-                            bottom: 0,
-                            top: 0
-                        }}
-                    />
-                </div>
+                    </div> :
+                    <div style={{ width: "100%"}}>
+                        <video
+                            ref={video}
+                            autoPlay
+                            muted
+                            onPlay={onPlay}
+                            style={{
+                                width: "100%",
+                                left: 0,
+                                right: 0,
+                                bottom: 0,
+                                top: 0
+                            }}
+                        />
+                    </div>
             }
             {
-                running && voterInfo && !voterInfo.hasVoted?
-                <div className={pageStyle}>
-                {
-                    candidates.filter(candadate => candadate.constituency === voterInfo.constituency).map(candidate =>
-                        <DocumentCard
-                            key={candidate.candidateId}
-                            styles={cardStyles}
-                            onClick={() => voteTo(candidate.candidateId)}
-                        >
-                            <DocumentCardDetails>
-                                <DocumentCardTitle title={candidate.candidateId + ": " + candidate.name} shouldTruncate />
-                            </DocumentCardDetails>
-                        </DocumentCard>)
-                }
-            </div>
-                :
-            <div>
-                Currently unavaiable
+                running && voterInfo && !voterInfo.hasVoted ?
+                    <div className={pageStyle}>
+                        {
+                            candidates.filter(candadate => candadate.constituency === voterInfo.constituency).map(candidate =>
+                                <DocumentCard
+                                    key={candidate.candidateId}
+                                    styles={cardStyles}
+                                    onClick={() => voteTo(candidate.candidateId)}
+                                >
+                                    <DocumentCardDetails>
+                                        <DocumentCardTitle title={candidate.candidateId + ": " + candidate.name} shouldTruncate />
+                                    </DocumentCardDetails>
+                                </DocumentCard>)
+                        }
+                    </div>
+                    :
+                    <div>
+                        Currently unavaiable
                 </div>
             }
-        </>
+        </div>
     )
 }
 
